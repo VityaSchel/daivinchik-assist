0c6876b3993be8cd2c71229b5d8916710a384ffe
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2022-10-01 16:35:33 +0400

    Finally fixed Metro in order to have single codebase for mobile & standalone

--- metro.config.js
+++ metro.config.js
@@ -1,16 +1,10 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { getDefaultConfig } = require('expo/metro-config')
 const exclusionList = require('metro-config/src/defaults/exclusionList')
+const fs = require('fs')
 
 const config = getDefaultConfig(__dirname)
 
 config.resolver.blacklistRE = exclusionList([/node_modules\/electron\/.*/, /src\/electron-wrapper\.ts$/])
 
 module.exports = config
-{
-  ...,
-  resolver: {
-    blacklistRE: exclusionList([/node_modules\/electron\/.*/, /src\/electron-wrapper\.ts$/])
-  },
-  transform
-}
--- src/screens/login/phone.tsx
+++ src/screens/login/phone.tsx
@@ -2,37 +2,34 @@ import React from 'react'
 import { StatusBar } from 'expo-status-bar'
 import { Button, StyleSheet, TextInput, Text, View } from 'react-native'
 import electron from 'electron'
-// const electron = null // #WEBPACK_REPLACE#
-import electronWrapper from '../../electron-wrapper'
 import { useNavigation } from '@react-navigation/native'
 
-// let electron
-// if(false) {
-//   electron = electronWrapper
-// }
+const usingElectron = false
 
 export default function LoginPhoneScreen() {
   const [phone, setPhone] = React.useState('')
   const navigation = useNavigation()
 
-  // React.useEffect(() => {
-  //   const callback = () => {
-  //     navigation.push('LoginCode')
-  //   }
+  React.useEffect(() => {
+    if(usingElectron) {
+      const callback = () => {
+        navigation.push('LoginCode')
+      }
 
-  //   ipcRenderer.on('login_phone_result', callback)
+      electron.ipcRenderer.on('login_phone_result', callback)
 
-  //   return () => {
-  //     ipcRenderer.removeListener('login_phone_result', callback)
-  //   }
-  // }, [])
+      return () => {
+        electron.ipcRenderer.removeListener('login_phone_result', callback)
+      }
+    }
+  }, [])
 
   return (
     <View style={{ display: 'flex', flexDirection: 'column' }}>
       <Text>Привет!</Text>
       <Text>Введи номер телефона от Telegram</Text>
       <TextInput value={phone} onChangeText={setPhone} style={{ borderStyle: 'solid', borderColor: 'black', borderWidth: 1 }} />
-      <Button /*onPress={() => ipcRenderer.send('login_phone', phone)}*/ title='Войти' />
+      <Button onPress={() => usingElectron && electron.ipcRenderer.send('login_phone', phone)} title='Войти' />
       <StatusBar style="auto" />
     </View>
   )
