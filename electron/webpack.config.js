f429e15c4af2b6f335740d8aff98068ba9dfd5ab
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2022-10-01 20:46:09 +0400

    ass pain

--- electron/webpack.config.js
+++ electron/webpack.config.js
@@ -1,6 +1,9 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 const { withExpoWebpack } = require('@expo/electron-adapter')
+const fs = require('fs')
 
 module.exports = config => {
-  return withExpoWebpack(config)
+  const config1 = withExpoWebpack(config)
+  config1.module.rules.unshift({ test: /\.scss$/, use: ['style-loader','css-loader','sass-loader']})
+  return config1
 }
