2cc710f0ef75852831c4a0fde40e4a2c65f36652
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2022-10-01 21:42:11 +0400

    i don't like it too

--- electron/webpack.config.js
+++ electron/webpack.config.js
@@ -2,18 +2,5 @@
 const { withExpoWebpack } = require('@expo/electron-adapter')
 
 module.exports = config => {
-  const config1 = withExpoWebpack(config)
-  config1.module.rules.push({ 
-    test: /\.tsx?$/, 
-    use: [
-      { 
-        loader: 'string-replace-loader',
-        options: {
-          search: /style={styles\.([a-zA-Z_0-9]+)}/,
-          replace: 'style={{ "--realClassName": "$1" }}',
-        }
-      }
-    ]
-  })
-  return config1
+  return withExpoWebpack(config)
 }
--- src/Container.tsx
+++ src/Container.tsx
@@ -3,7 +3,6 @@ import { SafeAreaView, StyleSheet, View } from 'react-native'
 import styles from './styles/globals.module.scss'
 
 export default function Container(props: { children: React.ReactNode }) {
-  console.log(styles.container)
   return (
     <SafeAreaView>
       <View style={styles.container}>
