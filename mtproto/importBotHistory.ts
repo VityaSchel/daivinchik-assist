b16d343b414c48d710d713a80517c1d190496c85
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2022-10-03 20:38:32 +0400

    Download & resume download bot history

--- mtproto/importBotHistory.ts
+++ mtproto/importBotHistory.ts
@@ -1,44 +1,62 @@
 type Peer = { id: string, access_hash: string }
 
-export async function findLeomatchPeer(): Promise<{ error: 'unable_to_resolve_peer' } | { peer: Peer, error: null }> {
-  // try {
-  const peer = await global.api.call('contacts.resolveUsername', {
-    username: 'leomatchbot'
-  })
-  if(peer._ === 'contacts.resolvedPeer') {
-    const bot = peer.users[0]
-    if(!bot) {
-      return { error: 'unable_to_resolve_peer' }
-    } else {
+export async function findLeomatchPeer(): Promise<{ error: 'unable_to_resolve_peer' | string } | { peer: Peer, error: null }> {
+  if(process.env.NODE_ENV === 'development') return { peer: { access_hash: '5955963651186977509', id: '1234060895' }, error: null }
+  
+  try {
+    const peer = await global.api.call('contacts.resolveUsername', {
+      username: 'leomatchbot'
+    })
+    if(peer._ === 'contacts.resolvedPeer') {
+      const bot = peer.users[0]
+      if(!bot) {
+        return { error: 'unable_to_resolve_peer' }
+      } else {
 
-      return { peer: { access_hash: bot.access_hash, id: bot.id }, error: null }
+        return { peer: { access_hash: bot.access_hash, id: bot.id }, error: null }
+      }
+    } else {
+      return { error: 'unable_to_resolve_peer' }
     }
-  } else {
-    return { error: 'unable_to_resolve_peer' }
+  } catch(e) {
+    return { error: JSON.stringify(e) }
   }
-  // } catch(e) {
-  //   if(e['error_message'] === 'USERNAME_NOT_OCCUPIED') {
-  //     return {}
-  //   }
-  // }
 }
 
-export async function exportHistory(leomatchPeer: Peer, callback: (exported: number) => any, offset_?: number) {
-  const pageSize = 100
+export async function exportHistory(leomatchPeer: Peer, callback: (exported: number, max: number, offset: number) => any, offset_?: number) {
+  const pageSize = 5
+  const messagesList = []
   let offset = Number.isInteger(offset_) ? offset_ : undefined
+  let max
 
-  const history = await global.api.call('messages.getHistory', {
-    peer: {
-      _: 'inputPeerUser',
-      user_id: leomatchPeer.id,
-      access_hash: leomatchPeer.access_hash,
-    },
-    offset_id: offset,
-    limit: pageSize
-  })
-  if(offset === undefined) offset = pageSize
-  else offset += pageSize
-  callback(offset)
+  do {
+    console.log('Calling export history with offset', offset, 'and limit', pageSize)
+    const history = await global.api.call('messages.getHistory', {
+      peer: {
+        _: 'inputPeerUser',
+        user_id: leomatchPeer.id,
+        access_hash: leomatchPeer.access_hash,
+      },
+      offset_id: offset,
+      limit: pageSize
+    })
+    const messages = history.messages
+    console.log('Saved', messages.length, 'messages')
 
-  console.log(history)
+    if(offset === undefined) max = history.count
+
+    if(messages.length > 0) {
+      offset = messages[messages.length - 1].id
+    }
+
+    callback(messagesList.length, max, offset ?? 0)
+    
+    if(messages.length > 0)
+      messagesList.push(...messages)
+    else
+      break
+
+  } while(messagesList.length < max && messagesList.length < 10)
+
+  console.log(JSON.stringify(messagesList))
 }
--- src/components/Feed/LoadHistory.tsx
+++ src/components/Feed/LoadHistory.tsx
@@ -1,27 +1,100 @@
 import React from 'react'
 import { View, Image } from 'react-native'
-import { Text, Button } from 'react-native-paper'
-import { findLeomatchPeer } from '../../../mtproto/importBotHistory'
+import { ProgressBar, Button, HelperText } from 'react-native-paper'
+import { exportHistory, findLeomatchPeer } from '../../../mtproto/importBotHistory'
 import AboutDialog from './AboutDialog'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 
 export default function LoadHistory() {
   const [howItWorksDialogVisible, setHowItWorksDialogVisibility] = React.useState(false)
+  const [loading, setLoading] = React.useState(false)
+  const [error, setError] = React.useState<null | string>(null)
+  const [progress, setProgress] = React.useState(0)
+  const [continueFrom, setContinueFrom] = React.useState<null | { exported: number, max: number, offset: number | undefined }>(null)
+  const [exporting, setExporting] = React.useState(false)
 
   const loadHistory = async () => {
-    await findLeomatchPeer()
+    setLoading(true)
+    setError(null)
+
+    const result = await findLeomatchPeer()
+    if(result.error) {
+      setLoading(false)
+      setError({
+        'unable_to_resolve_peer': 'Не удалось получить peer бота Дайвинчик. Возможно, он был удален из Telegram или вы используете устаревшую версию приложения.'
+      }[result.error] ?? result.error)
+    } else {
+      setExporting(true)
+      await AsyncStorage.setItem('init_history_export_state', 'started')
+      const callback = (exported: number, max: number, offset: number) => {
+        setProgress(exported / max)
+        AsyncStorage.setItem('init_history_exported_msgs_process', JSON.stringify({ exported, max, offset }))
+      }
+      await exportHistory(result.peer, callback, continueFrom?.offset)
+    }
+  }
+
+  React.useEffect(() => { checkState() }, [])
+
+  const checkState = async () => {
+    setLoading(true)
+    const state = await AsyncStorage.getItem('init_history_export_state')
+    if(state === 'started') {
+      const dataRaw = await AsyncStorage.getItem('init_history_exported_msgs_process')
+      if(!dataRaw) {
+        setContinueFrom(null)
+      } else {
+        try {
+          const data = JSON.parse(dataRaw)
+          setContinueFrom({
+            exported: Number(data.exported) || 0,
+            max: Number(data.max) || 1,
+            offset: Number(data.offset) || undefined
+          })
+        } catch(e) {
+          setContinueFrom(null)
+        }
+      }
+    } else {
+      setContinueFrom(null)
+    }
+    setLoading(false)
   }
   
   return (
     <View>
-      <Button 
-        mode='contained'
-        onPress={loadHistory}
-      >Загрузить</Button>
+      {!exporting && 
+      (continueFrom === null 
+        ? (
+          <Button 
+            mode='contained'
+            onPress={loadHistory}
+            disabled={loading}
+          >
+            Начать загрузку
+          </Button>
+        ) : (
+          <Button 
+            mode='contained'
+            onPress={loadHistory}
+            disabled={loading}
+          >
+            Продолжить загрузку ({Math.floor(continueFrom.exported/continueFrom.max)}%)
+          </Button>
+        )
+      )
+      }
+      {Boolean(error) && <HelperText type='error' visible={Boolean(error)}>{error}</HelperText>}
+      {exporting && <ProgressBar progress={progress} />}
       <Button 
         mode='outlined'
         onPress={() => setHowItWorksDialogVisibility(true)}
       >Как это работает?</Button>
       <AboutDialog visible={howItWorksDialogVisible} onHide={() => setHowItWorksDialogVisibility(false)} />
+      <Button 
+        mode='outlined'
+        onPress={() => setLoading(false)}
+      >[[ undo ]]</Button>
     </View>
   )
 }
