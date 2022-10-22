37d827155ed8680cd3da8bf53f8bb3fdbf61d2b1
VityaSchel <59040542+VityaSchel@users.noreply.github.com>
2022-10-22 22:02:07 +0400

    Add constants to BotMessageType, detect message type function

--- src/models/Message.ts
+++ src/models/Message.ts
@@ -1,23 +1,79 @@
 import { Realm } from '@realm/react'
 
+type BotMessageType = 
+  /** Reserved for future use */
+    'interface'
+  /** "Кому-то понравилась твоя анкета(и еще 2)" */
+  | 'incoming_like' 
+  /** "Отлично! Надеюсь хорошо проведете время ;) Начинай общаться 👉" */
+  | 'like_response'
+  /** Reserved for future use */
+  | 'ads'
+  /** "Имя, 📍900м — Описание" */
+  | 'candidate_profile'
+  /** Reserved for future use */
+  | 'self_profile'
+  /** Reserved for future use */
+  | 'self_profile_setup_age_question'
+  /** Reserved for future use */
+  | 'self_profile_setup_age_answer'
+  /** Reserved for future use */
+  | 'self_profile_setup_target_sex_question'
+  /** Reserved for future use */
+  | 'self_profile_setup_target_sex_answer'
+  /** Reserved for future use */
+  | 'self_profile_setup_city_question'
+  /** Reserved for future use */
+  | 'self_profile_setup_city_answer'
+  /** Reserved for future use */
+  | 'self_profile_setup_name_question'
+  /** Reserved for future use */
+  | 'self_profile_setup_name_answer'
+  /** Any other type that could not be detected. Must be ignored */
+  | 'unknown'
+
+const userProfileRegex = '^(.+?), (\\d+), (.+?)( – ([\\s\\S]+?))?(\\n\\nСообщение для тебя💌: ([\\s\\S]+))?$'
+const incomingLikeMessageRegex = '^Кому-то понравилась твоя анкета(:|\\(и еще \\d+\\))?\n\n' + userProfileRegex
+const likeResponseRegex = '^Отлично! Надеюсь хорошо проведете время ;\\) Начинай общаться 👉 (.+)$'
+
+interface MessageFields {
+  _id: Realm.BSON.ObjectId
+  type: BotMessageType
+  text: string
+}
+
 export class Message extends Realm.Object {
   _id!: Realm.BSON.ObjectId
+  type!: BotMessageType
   text!: string
 
-  static generate(message: object) {
+  static generate(message: object): MessageFields {
     return {
       _id: new Realm.BSON.ObjectId(),
-      text
+      type: message['out'] ? 'unknown' : detectMessageType(message),
+      text: message['message']
     }
   }
 
-  // To use a class as a Realm object type, define the object schema on the static property "schema".
   static schema = {
     name: 'Message',
     primaryKey: '_id',
     properties: {
       _id: 'objectId',
+      type: 'string',
       text: 'string'
     },
+  }
+}
+
+function detectMessageType(message: object): BotMessageType {
+  if(new RegExp(incomingLikeMessageRegex).test(message['message'])) {
+    return 'incoming_like'
+  } else if(new RegExp(userProfileRegex).test(message['message'])) {
+    return 'candidate_profile'
+  } else if(new RegExp(likeResponseRegex).test(message['message'])) {
+    return 'like_response'
+  } else {
+    return 'unknown'
   }
 }
